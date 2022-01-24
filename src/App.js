9a87b832185e2a2f91fe096736b5226ad1f9226c
Tamirchn1@gmail.com <T2151991t>
2022-01-24 09:32:43 +0100

    added redirect file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 import Home from "./components/Home"
