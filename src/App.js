165bffda5c49857c4b509586ec1ea968b588c1b4
Tamirchn1@gmail.com <T2151991t>
2022-01-24 09:15:17 +0100

    add routing, back button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 
 
 import Home from "./components/Home"
@@ -14,21 +9,21 @@ import PrivateLesson from "./components/Privatelesson"
 import Workshop from "./components/Workshop"
 import Tetra from "./components/Tetra"
 
+ 
+function App(){
+  return (
+    <Router> 
+       <div className='App'>
+         <Route exact path="/" component={Home}/>
+         <Route exact path="/about" component={About}/>
+         <Route exact path="/PrivateWorkshop" component={PrivateWorkshop}/>
+         <Route exact path="/PrivateLesson" component={PrivateLesson}/>
+         <Route exact path="/Workshop" component={Workshop}/>
+         <Route exact path="/Tetra" component={Tetra}/>
 
-class App extends Component {
-  render() {
-    return (
-      <div> 
-      <Home/>
-      <About/>
-      <PrivateWorkshop/>
-      <PrivateLesson/>
-      <Workshop/>
-      <Tetra/>
-      </div>
-     
-    );
-  }
+    </div></Router>
+  
+  )
 }
-
+ 
 export default App;
