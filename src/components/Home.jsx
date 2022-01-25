681559297f5539065881c11652a18f8e3b90c037
Tamirchn1@gmail.com <T2151991t>
2022-01-25 16:29:45 +0100

    added copyright

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header"
 import Menu from "./Menu"
+import Copyright from "./Copyright"
 
 
 function Home() {
    return(
-   <div>
+   <div className= "mt-1">
        <Header/>
        <Menu/>
+       <Copyright/>
       </div>);
 }
 export default Home;
